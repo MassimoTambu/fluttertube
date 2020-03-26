c8d9e27fa4201bf45283e3bb8d4932439c385b8d
Massimo Tamburini <massimo.tamburini.work@gmail.com>
2020-03-27 00:13:48 +0100

    Added radio quality
    Co-authored-by: Lorenzo Polverelli <lorenzo.polverelli13@gmail.com>

--- lib/download_tab.dart
+++ lib/download_tab.dart
@@ -10,12 +10,14 @@ class DownloadTab extends StatefulWidget {
 }
 
 class _DownloadTabState extends State<DownloadTab> {
-  String _error;
   bool _audioOnly = false;
   bool _dowloading = false;
+  String _searchUrl = '';
   Stream<List<int>> stream;
-  List<yt.AudioStreamInfo> bitrates;
-  List<yt.MuxedStreamInfo> resolutions;
+  yt.MediaStreamInfoSet _media;
+  yt.MediaStreamInfo _selectedStream;
+  String id;
+  String path;
 
   onChangeSwitch(bool audioOnly) {
     setState(() {
@@ -25,39 +27,45 @@ class _DownloadTabState extends State<DownloadTab> {
 
   void onSubmit(String url) async {
     setState(() {
-      _error = null;
       _dowloading = true;
     });
     try {
       final yte = yt.YoutubeExplode();
-      final id = yt.YoutubeExplode.parseVideoId(url);
-      final video = await yte.getVideoMediaStream(id);
+      id = yt.YoutubeExplode.parseVideoId(url);
+      _media = await yte.getVideoMediaStream(id);
       final dir = await getExternalStorageDirectory();
-      final path = dir.path;
-      File file;
-      if (_audioOnly) {
-        // stream = video.audio[video.audio.length - 1].downloadStream();
-        bitrates = video.audio;
-        file = File('$path/$id.ogg');
-      } else {
-        // stream = video.muxed[video.muxed.length - 1].downloadStream();
-        resolutions = video.muxed;
-        file = File('$path/$id.mp4');
-      }
+      path = dir.path;
+      yte.close();
+    } catch (e) {
+      _errorDialog(context, 'Errore', e.message);
+    }
+    setState(() {
+      _dowloading = false;
+    });
+  }
+
+  void _download(yt.MediaStreamInfo download) async {
+    setState(() {
+      _dowloading = true;
+    });
+    File file;
+    if (_audioOnly) {
+      file = File('$path/$id.ogg');
+    } else {
+      file = File('$path/$id.mp4');
+    }
+    try {
       if (await file.exists()) {
         if (!await _confirmOverride(context)) {
           return;
         }
         file.writeAsBytesSync([]);
       }
-      await for (var value in stream) {
+      await for (var value in download.downloadStream()) {
         await file.writeAsBytes(value, mode: FileMode.append);
       }
-      yte.close();
     } catch (e) {
-      setState(() {
-        _error = e.toString();
-      });
+      _errorDialog(context, 'Errore', e.message);
     } finally {
       setState(() {
         _dowloading = false;
@@ -65,24 +73,43 @@ class _DownloadTabState extends State<DownloadTab> {
     }
   }
 
+  Future<void> _errorDialog(
+      BuildContext context, String title, String content) {
+    return showDialog<bool>(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+      barrierDismissible: false,
+    );
+  }
+
   Future<bool> _confirmOverride(BuildContext context) {
     return showDialog<bool>(
-        context: context,
-        builder: (_) => AlertDialog(
-              title: Text('Attenzione'),
-              content: Text('Questo file esiste già, vuoi sovrascriverlo?'),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text('No'),
-                  onPressed: () => Navigator.of(context).pop(false),
-                ),
-                FlatButton(
-                  child: Text('Sì'),
-                  onPressed: () => Navigator.of(context).pop(true),
-                ),
-              ],
-            ),
-        barrierDismissible: false);
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Attenzione'),
+        content: Text('Questo file esiste già, vuoi sovrascriverlo?'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('No'),
+            onPressed: () => Navigator.of(context).pop(false),
+          ),
+          FlatButton(
+            child: Text('Sì'),
+            onPressed: () => Navigator.of(context).pop(true),
+          ),
+        ],
+      ),
+      barrierDismissible: false,
+    );
   }
 
   @override
@@ -91,13 +118,44 @@ class _DownloadTabState extends State<DownloadTab> {
       padding: const EdgeInsets.all(12),
       child: Column(
         children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Url',
-            ),
-            onSubmitted: onSubmit,
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Url',
+                  ),
+                  onChanged: (url) {
+                    _searchUrl = url;
+                  },
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () => onSubmit(_searchUrl),
+              )
+            ],
           ),
+          if (_media != null)
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      _media.videoDetails.title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text('Durata: ' +
+                        (new RegExp(r"(.+)\.\d+$"))
+                            .firstMatch(_media.videoDetails.duration.toString())
+                            .group(1)),
+                  ],
+                ),
+              ),
+            ),
           Row(
             children: <Widget>[
               SizedBox(height: 20),
@@ -111,7 +169,56 @@ class _DownloadTabState extends State<DownloadTab> {
               ),
             ],
           ),
-          if (_error != null) Text(_error),
+          if (_audioOnly && _media != null)
+            Column(
+              children: _media.audio.map((a) {
+                return Row(
+                  children: <Widget>[
+                    Radio(
+                      value: a,
+                      groupValue: _selectedStream,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedStream = value;
+                        });
+                      },
+                    ),
+                    Text(
+                      'Bitrate: ${a.bitrate / 1000} kb - Peso: ${a.size / 1000000} MB',
+                      style: TextStyle(fontSize: 13),
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+          if (!_audioOnly && _media != null)
+            Column(
+              children: _media.muxed.map((v) {
+                return Row(
+                  children: <Widget>[
+                    Radio(
+                      value: v,
+                      groupValue: _selectedStream,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedStream = value;
+                        });
+                      },
+                    ),
+                    Text(
+                        'Qualità: ${v.videoQualityLabel} - Peso: ${v.size / 1000000} MB'),
+                  ],
+                );
+              }).toList(),
+            ),
+          if (_media != null)
+            Align(
+              alignment: Alignment.centerRight,
+              child: RaisedButton(
+                child: Text('Scarica'),
+                onPressed: () => _download(_selectedStream),
+              ),
+            ),
           if (_dowloading) CircularProgressIndicator()
         ],
       ),
